27590c7d337eee38715ad8da203f3abffbd601e3
Rupali <Rupali@LAPTOP-F0VEO623>
2024-09-26 09:45:18 +0530

    by pragati

--- Github_Day_1_Demo/src/com/cjc/Day1/Demo.java
+++ Github_Day_1_Demo/src/com/cjc/Day1/Demo.java
@@ -1,6 +1,9 @@
 package com.cjc.Day1;
 
 public class Demo {
+	public void m1() {
+		System.out.println("m1 of demo");
+	}
 public static void main(String[] args) {
 	System.out.println("hello welcome to github");
 }
