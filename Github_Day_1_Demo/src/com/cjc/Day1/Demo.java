a71a6eda3342ba91e713089698f2bc7fd62622ee
pragatiburele <pragatiburele@gmail.com>
2024-09-26 09:50:35 +0530

    Update Demo.java
    i added m2 method

--- Github_Day_1_Demo/src/com/cjc/Day1/Demo.java
+++ Github_Day_1_Demo/src/com/cjc/Day1/Demo.java
@@ -4,6 +4,9 @@ public class Demo {
 	public void m1() {
 		System.out.println("m1 of demo");
 	}
+	public void m2(){
+		System.out.println("m2 of demo")
+	}
 public static void main(String[] args) {
 	System.out.println("hello welcome to github");
 }
